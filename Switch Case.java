c7e193d4fa01ee11ab0c91c64889e191c03b643e
Himal Mevada <63632500+Himalmevada@users.noreply.github.com>
2020-09-22 20:18:27 +0530

    Switch Case
    Here I uploaded How Switch Case Works.
    This is just simple program where I given three Case.

---
+++ Switch Case.java
@@ -0,0 +1,25 @@
+public class MinutesIntoYears {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int select;
+    System.out.println("Enter Your Choice: ");
+    System.out.println("1. Hii\n 2. Hey\n 3. Hello");
+		switch(select)
+    {
+       case 1 : System.out.println("You said Hii");
+                break;
+      
+       case 2 : System.out.println("You said Hey");
+                break;
+    
+       case 3 : System.out.println("You said Hello");
+                break;
+                
+       default : System.out.println("Invalid Choice");
+    }
+
+	}
+
+}
